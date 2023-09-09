a4152f63c2444d8e53c16f2f23c34a305a2536ff
Donald Udochukwu <104968687+Udoc-max@users.noreply.github.com>
2023-09-09 04:40:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 app.get("/api", (req, res) => {
-  const { slack_name, track } = req.query;
+  const { track } = req.query; // Assuming slack_name is hardcoded
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const currentDay = daysOfWeek[new Date().getDay()];
   const now = new Date();
-  const utc = now.toISOString().slice(0, 19) + "Z"; // Use 'now' instead of 'date'
+  const utc = now.toISOString().slice(0, 19) + "Z";
 
   const responseApi = {
-    slack_name: Udochukwu,
+    slack_name: "Udochukwu", // Set slack_name to "Udochukwu"
     current_day: currentDay,
     utc_time: utc,
     track: track,
@@ -29,3 +29,4 @@ app.get("/api", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
