572a24c52c13dbc75bcdbebcfaff6f3563e26a95
Donald Udochukwu <104968687+Udoc-max@users.noreply.github.com>
2023-09-09 04:31:18 -0700

    Update app.js
    changed the code to correct the time

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.get("/api", (req, res) => {
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const currentDay = daysOfWeek[new Date().getDay()];
   const now = new Date();
-  const utcTime = new Date(now.getTime() + (now.getTimezoneOffset() * 60000)).toISOString();
-  
+  const utc = now.toISOString().slice(0, 19) + "Z"; // Use 'now' instead of 'date'
+
   const responseApi = {
     slack_name: slack_name,
     current_day: currentDay,
-    utc_time: utcTime,
+    utc_time: utc,
     track: track,
     github_file_url: "https://github.com/Udoc-max/Query-api/blob/main/app.js",
     github_repo_url: "https://github.com/Udoc-max/Query-api",
